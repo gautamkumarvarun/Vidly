bdb14497d70fdc5b68d7ec8468a33dccad71dee3
gautamkumarvarun <41418355+gautamkumarvarun@users.noreply.github.com>
2019-09-23 16:58:19 +0530

    Add files via upload

---
+++ MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly1.Models;
+
+namespace Vidly1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult MovieList()
+        {
+            var movie = _context.Movies.ToList();
+
+            return View(movie);
+
+            //return RedirectToAction("errorPage","home");
+        }
+
+        public ActionResult MovieDetails(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m=>m.Id==id);
+            
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            return View(movie);
+
+            //return RedirectToAction("errorPage","home");
+        }
+
+    }
+}
